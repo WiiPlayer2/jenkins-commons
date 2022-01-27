eeba5a94a091c8efd85c766b434a90e78e913803
Waldemar Tomme <waldetomme@gmail.com>
2022-01-27 12:58:42 +0100

    Remove match method

--- dockerBuild.groovy
+++ dockerBuild.groovy
@@ -1,31 +1,17 @@
 def build(config)
 {
-    this.match(
-        onLocalPlatform:
-        {
-            sh "docker build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} ."
-        },
-        onMultiPlatform:
-        {
-            def buildInstance = sh (
-                script: "docker buildx create",
-                returnStdout: true
-            ).trim();
-            sh "docker buildx use $buildInstance"
-            sh "docker buildx build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} ."
-        }
-    )
-}
-
-def match(onLocalPlatform, onMultiPlatform)
-{
-    if(config.containsKey('platforms') && config.platforms.size() > 0)
+    if(!config.containsKey('platforms') || config.platforms.size() == 0)
     {
-        onMultiPlatform();
+        sh "docker build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} ."
     }
     else
     {
-        onLocalPlatform();
+        def buildInstance = sh (
+            script: "docker buildx create",
+            returnStdout: true
+        ).trim();
+        sh "docker buildx use $buildInstance"
+        sh "docker buildx build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} ."
     }
 }
 
@@ -33,21 +19,19 @@ def publish(config)
 {
     withDockerRegistry([credentialsId: config.registryCredentials, url: "https://${config.registry}/"])
     {
-        this.match(
-            onLocalPlatform:
-            {
-                def buildInstance = sh (
-                    script: "docker buildx create",
-                    returnStdout: true
-                ).trim();
-                sh "docker buildx use $buildInstance"
-                sh "docker buildx build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} --push ."
-            },
-            onMultiPlatform:
-            {
-                sh "docker image push ${config.registry}/${config.imageName}:${config.tag}"
-            }
-        );
+        if(!config.containsKey('platforms') || config.platforms.size() == 0)
+        {
+            def buildInstance = sh (
+                script: "docker buildx create",
+                returnStdout: true
+            ).trim();
+            sh "docker buildx use $buildInstance"
+            sh "docker buildx build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} --push ."
+        }
+        else
+        {
+            sh "docker image push ${config.registry}/${config.imageName}:${config.tag}"
+        }
     }
 }
 
