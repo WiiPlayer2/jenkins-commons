d9307f20a8c0e2b6407301a57f0111140598a19a
Waldemar Tomme <waldetomme@gmail.com>
2022-01-27 15:41:54 +0100

    Add buildAndPublish

--- dockerBuild.groovy
+++ dockerBuild.groovy
@@ -1,37 +1,60 @@
+def buildLocalPlatform(config)
+{
+    sh "docker build -t ${config.fullTag} --pull -f ${config.dockerfile} ."
+}
+
+def buildMultiPlatform(config)
+{
+    def buildInstance = sh (
+        script: "docker buildx create",
+        returnStdout: true
+    ).trim();
+    sh "docker buildx use $buildInstance"
+    sh "docker buildx build -t ${config.fullTag} --cache-to=type=local,dest=${config.buildCache} --pull -f ${config.dockerfile} --platform ${config.platforms.join(',')} ."
+}
+
 def build(config)
 {
     if(config.platforms.size() == 0)
     {
-        sh "docker build -t ${config.fullTag} --pull -f ${config.dockerfile} ."
+        buildLocalPlatform();
     }
     else
+    {
+        buildMultiPlatform();
+    }
+}
+
+def publishLocalPlatform(config)
+{
+    withDockerRegistry([credentialsId: config.registryCredentials, url: "https://${config.registry}/"])
+    {
+        sh "docker image push ${config.fullTag}"
+    }
+}
+
+def publishMultiPlatform(config)
+{
+    withDockerRegistry([credentialsId: config.registryCredentials, url: "https://${config.registry}/"])
     {
         def buildInstance = sh (
             script: "docker buildx create",
             returnStdout: true
         ).trim();
         sh "docker buildx use $buildInstance"
-        sh "docker buildx build -t ${config.fullTag} --cache-to=type=local,dest=${config.buildCache} --pull -f ${config.dockerfile} --platform ${config.platforms.join(',')} ."
+        sh "docker buildx build -t ${config.fullTag} --cache-from=type=local,src=${config.buildCache} --pull -f ${config.dockerfile} --platform ${config.platforms.join(',')} --push ."
     }
 }
 
 def publish(config)
 {
-    withDockerRegistry([credentialsId: config.registryCredentials, url: "https://${config.registry}/"])
+    if(config.platforms.size() == 0)
     {
-        if(config.platforms.size() == 0)
-        {
-            sh "docker image push ${config.fullTag}"
-        }
-        else
-        {
-            def buildInstance = sh (
-                script: "docker buildx create",
-                returnStdout: true
-            ).trim();
-            sh "docker buildx use $buildInstance"
-            sh "docker buildx build -t ${config.fullTag} --cache-from=type=local,src=${config.buildCache} --pull -f ${config.dockerfile} --platform ${config.platforms.join(',')} --push ."
-        }
+        publishLocalPlatform(config);
+    }
+    else
+    {
+        publishMultiPlatform(config);
     }
 }
 
@@ -48,6 +71,26 @@ def prepare(config)
     }
 
     config.fullTag = "${config.registry}/${config.imageName}:${config.tag}";
+}
+
+def buildAndPublish(config)
+{
+    if(config.platforms.size() == 0)
+    {
+        stage('Build') {
+            buildLocalPlatform(config);
+        }
+
+        stage('Publish') {
+            publishLocalPlatform(config);
+        }
+    }
+    else
+    {
+        stage('Build & Publish') {
+            publishMultiPlatform(config);
+        }
+    }
 }
 
 return this;
