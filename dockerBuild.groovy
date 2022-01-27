4fcdaf61a957e314dfabbcf57bf3cf4dc12eb392
Waldemar Tomme <waldetomme@gmail.com>
2022-01-27 13:24:03 +0100

    Fix publish

--- dockerBuild.groovy
+++ dockerBuild.groovy
@@ -21,16 +21,16 @@ def publish(config)
     {
         if(!config.containsKey('platforms') || config.platforms.size() == 0)
         {
+            sh "docker image push ${config.registry}/${config.imageName}:${config.tag}"
+        }
+        else
+        {
             def buildInstance = sh (
                 script: "docker buildx create",
                 returnStdout: true
             ).trim();
             sh "docker buildx use $buildInstance"
             sh "docker buildx build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} --platform ${config.platforms.join(',')} --push ."
-        }
-        else
-        {
-            sh "docker image push ${config.registry}/${config.imageName}:${config.tag}"
         }
     }
 }
