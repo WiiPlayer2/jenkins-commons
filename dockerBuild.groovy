bde89f957ceb2d7347364e3a5974ff94fc88ff8a
Waldemar Tomme <waldetomme@gmail.com>
2022-01-27 12:51:25 +0100

    Add multiplatform support

--- dockerBuild.groovy
+++ dockerBuild.groovy
@@ -1,13 +1,53 @@
 def build(config)
 {
-    sh "docker build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} ."
+    match(
+        onLocalPlatform:
+        {
+            sh "docker build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} ."
+        },
+        onMultiPlatform:
+        {
+            def buildInstance = sh (
+                script: "docker buildx create",
+                returnStdout: true
+            ).trim();
+            sh "docker buildx use $buildInstance"
+            sh "docker buildx build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} ."
+        }
+    )
+}
+
+private def match(onLocalPlatform, onMultiPlatform)
+{
+    if(config.containsKey('platforms') && config.platforms.size() > 0)
+    {
+        onMultiPlatform();
+    }
+    else
+    {
+        onLocalPlatform();
+    }
 }
 
 def publish(config)
 {
     withDockerRegistry([credentialsId: config.registryCredentials, url: "https://${config.registry}/"])
     {
-        sh "docker image push ${config.registry}/${config.imageName}:${config.tag}"
+        match(
+            onLocalPlatform:
+            {
+                def buildInstance = sh (
+                    script: "docker buildx create",
+                    returnStdout: true
+                ).trim();
+                sh "docker buildx use $buildInstance"
+                sh "docker buildx build -t ${config.registry}/${config.imageName}:${config.tag} --pull -f ${config.dockerfile} --push ."
+            },
+            onMultiPlatform:
+            {
+                sh "docker image push ${config.registry}/${config.imageName}:${config.tag}"
+            }
+        );
     }
 }
 
