803a1ab55e4ef71c618c98a3f1011bc2c8a7037d
Waldemar Tomme <waldetomme@gmail.com>
2022-02-24 21:52:57 +0100

    Add extraction of changeset

--- unity.groovy
+++ unity.groovy
@@ -13,6 +13,14 @@ def getProjectEditorVersion(projectPath)
     return content['m_EditorVersion'];
 }
 
+def getProjectEditorChangeset(projectPath)
+{
+    def projectVersionFilePath = "${projectPath}/ProjectSettings/ProjectVersion.txt";
+    def content = readYaml file: projectVersionFilePath;
+    def matches = content['m_EditorVersionWithRevision'] =~ /\(([0-9a-f]{12})\)/;
+    return matches[0][0];
+}
+
 def installEditor(version, changeset = null, modules = [])
 {
     if(modules == null)
