0d508ec20b8bd7cdc2dca84f6733cbea7dfdeeb3
Waldemar Tomme <waldetomme@gmail.com>
2022-03-30 16:47:30 +0200

    Add isTriggeredByCron

--- buildCauses.groovy
+++ buildCauses.groovy
@@ -1,5 +1,6 @@
 isTriggeredByIndexing = currentBuild.getBuildCauses('jenkins.branch.BranchIndexingCause').size();
 isTriggeredByCommit = currentBuild.getBuildCauses('com.cloudbees.jenkins.GitHubPushCause').size();
 isTriggeredByUser = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').size();
+isTriggeredByCron = currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size();
 
 return this;
