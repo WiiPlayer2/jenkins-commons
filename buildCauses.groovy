c8a6b7abc1ff3a1dcb87761dc106ed90da61723e
Waldemar Tomme <waldetomme@gmail.com>
2022-08-14 14:35:54 +0200

    Triggers are now boolean

--- buildCauses.groovy
+++ buildCauses.groovy
@@ -1,8 +1,8 @@
-isTriggeredByIndexing = currentBuild.getBuildCauses('jenkins.branch.BranchIndexingCause').size();
-isTriggeredByPush = currentBuild.getBuildCauses('com.cloudbees.jenkins.GitHubPushCause').size(); // better name
+isTriggeredByIndexing = currentBuild.getBuildCauses('jenkins.branch.BranchIndexingCause').size() > 0;
+isTriggeredByPush = currentBuild.getBuildCauses('com.cloudbees.jenkins.GitHubPushCause').size() > 0;
 isTriggeredByCommit = isTriggeredByPush; // Obsolete
-isTriggeredByUser = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').size();
-isTriggeredByCron = currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size();
+isTriggeredByUser = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').size() > 0;
+isTriggeredByCron = currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size() > 0;
 
 def selectManually()
 {
