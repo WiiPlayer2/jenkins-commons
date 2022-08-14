2ea52353673a01a5c758da94de097c25d34f3624
Waldemar Tomme <waldetomme@gmail.com>
2022-08-14 12:42:34 +0200

    Add causes match

--- buildCauses.groovy
+++ buildCauses.groovy
@@ -1,5 +1,6 @@
 isTriggeredByIndexing = currentBuild.getBuildCauses('jenkins.branch.BranchIndexingCause').size();
-isTriggeredByCommit = currentBuild.getBuildCauses('com.cloudbees.jenkins.GitHubPushCause').size();
+isTriggeredByPush = currentBuild.getBuildCauses('com.cloudbees.jenkins.GitHubPushCause').size(); // better name
+isTriggeredByCommit = isTriggeredByPush; // Obsolete
 isTriggeredByUser = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').size();
 isTriggeredByCron = currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size();
 
--- utils.groovy
+++ utils.groovy
@@ -1,3 +1,5 @@
+def causes = load "buildCauses.groovy"
+
 def doesMatch(matchConfig)
 {
     def result = true;
@@ -5,6 +7,12 @@ def doesMatch(matchConfig)
     if(config.containsKey('branch'))
     {
         result = result && env.BRANCH_NAME ==~ config.branch
+    }
+
+    if (config.containsKey('causes'))
+    {
+        result = result && (config.causes.contains('push') || causes.isTriggeredByPush);
+        result = result && (config.causes.contains('cron') || causes.isTriggeredByCron);
     }
 
     return result;
