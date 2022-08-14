aba1e40897aa0293890e7e45d7676214ebfcd11e
Waldemar Tomme <waldetomme@gmail.com>
2022-08-14 13:02:56 +0200

    Add selectManually

--- buildCauses.groovy
+++ buildCauses.groovy
@@ -4,4 +4,19 @@ isTriggeredByCommit = isTriggeredByPush; // Obsolete
 isTriggeredByUser = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').size();
 isTriggeredByCron = currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').size();
 
+def selectManually()
+{
+    def inputSelection = input message: 'Select emulated trigger cause', parameters: [choice(choices: ['User', 'Push', 'Cron'], name: 'Cause')]
+    switch(inputSelection)
+    {
+        case "Push":
+            isTriggeredByPush = true;
+            break;
+
+        case "Cron":
+            isTriggeredByCron = true;
+            break;
+    }
+}
+
 return this;
