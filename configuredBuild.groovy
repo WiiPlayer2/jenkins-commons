c8252f602c17741b2432cfa9ef9a7db6c5ac8296
Waldemar Tomme <waldetomme@gmail.com>
2023-04-12 21:22:03 +0200

    Restructure configuredBuild

--- configuredBuild.groovy
+++ configuredBuild.groovy
@@ -1,22 +1,51 @@
 def run(stageName)
 {
+    def config = _loadConfiguration();
+    _run(stageName, config);
+}
+
+def run()
+{
+    def config = _loadConfiguration();
+
+    for(stageName in config.builder.getStages())
+    {
+        stage(stageName)
+        {
+            _run(stageName, config);
+        }
+    }
+}
+
+def _loadConfiguration()
+{
     def configuration = readYaml file: "CI.yaml";
     def kv = configuration.entrySet().iterator().next(); // only support single configuration for now
     def configurationType = kv.key;
     def configurationData = kv.value;
 
     def builder = load "ci/jenkins/types/${configurationType}.groovy";
-    if(!builder.getStages().contains(stageName))
+
+    return [
+        type: configurationType,
+        data: configurationData,
+        builder: builder,
+    ];
+}
+
+def _run(stageName, config)
+{
+    if(!config.builder.getStages().contains(stageName))
         return;
 
-    echo "[$stageName] $configurationType: $configurationData";
+    echo "[$stageName] ${config.type}: ${config.data}";
 
     def preparedConfig = builder.createConfig();
-    preparedConfig.putAll(configurationData);
+    preparedConfig.putAll(config.data);
 
-    builder.preStage(stageName);
-    builder."$stageName"(preparedConfig);
-    builder.postStage(stageName);
+    config.builder.preStage(stageName);
+    config.builder."$stageName"(preparedConfig);
+    config.builder.postStage(stageName);
 }
 
 return this;
