28b1c2f255e3cf18857ca2a28232ee9cd6fa8817
Waldemar Tomme <waldetomme@gmail.com>
2023-04-22 15:48:52 +0200

    Implement local platform builds

--- types/Docker.groovy
+++ types/Docker.groovy
@@ -4,13 +4,16 @@
 def getStages()
 {
     return [
+        "Build",
         "Publish",
     ];
 }
 
 def createConfig()
 {
-    return [:];
+    return [
+        Platforms: [],
+    ];
 }
 
 def preStage(stageName) { }
@@ -20,27 +23,70 @@ def postStage(stageName) { }
 //--------
 // Stages
 //--------
+def Build(config) {
+    def fullTag = __getFullTag(config);
+
+    if(config.Platforms.size() == 0)
+    {
+        __buildLocalPlatform(config, fullTag);
+    }
+    else
+    {
+        __buildMultiPlatform(config, fullTag);
+    }
+}
+
 def Publish(config) {
-    def fullTag = "${config.Registry}/${config.Repository}:${config.Tag}"
+    def fullTag = __getFullTag(config);
 
     withDockerRegistry([credentialsId: config.RegistryCredentials, url: "https://${config.Registry}/"])
     {
-        sh "docker buildx create --name $BUILD_TAG --use"
+        if(config.Platforms.size() == 0)
+        {
+            __publishLocalPlatform(config, fullTag);
+        }
+        else
+        {
+            __publishMultiPlatform(config, fullTag);
+        }
+    }
+}
 
-        try
-        {
-            sh """docker buildx build \\
-                    -t ${fullTag} \\
-                    --pull \\
-                    -f ${config.Dockerfile} \\
-                    --platform ${config.Platforms.join(',')} \\
-                    --push \\
-                    ."""
-        }
-        finally
-        {
-            sh "docker buildx rm --force $BUILD_TAG"
-        }
+//---------
+// Helpers
+//---------
+def __getFullTag(config) {
+    return "${config.Registry}/${config.Repository}:${config.Tag}";
+}
+
+def __buildLocalPlatform(config, fullTag) {
+    sh "docker build -t ${fullTag} --pull -f ${config.dockerfile} .";
+}
+
+def __publishMultiPlatform(config, fullTag) {
+    sh "docker image push ${fullTag}";
+}
+
+def __buildMultiPlatform(config, fullTag) {
+    echo "When building a multiplatform image the build stage does nothing.";
+}
+
+def __publishMultiPlatform(config, fullTag) {
+    sh "docker buildx create --name $BUILD_TAG --use";
+
+    try
+    {
+        sh """docker buildx build \\
+                -t ${fullTag} \\
+                --pull \\
+                -f ${config.Dockerfile} \\
+                --platform ${config.Platforms.join(',')} \\
+                --push \\
+                .""";
+    }
+    finally
+    {
+        sh "docker buildx rm --force $BUILD_TAG";
     }
 }
 
