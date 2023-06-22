6ac2be35d25b1f497b8155ccced5a56d1dad7bca
Waldemar Tomme <waldetomme@gmail.com>
2023-06-22 20:46:54 +0200

    Add temporary fix for stage order

---
+++ types/Stages_FIX.groovy
@@ -0,0 +1,32 @@
+//--------------
+// Core methods
+//--------------
+def metadata = [
+    WrapStage: false,
+    FixedStages: false,
+];
+
+def createConfig()
+{
+    return [:];
+}
+
+def getStages(config)
+{
+    return config.Stages;
+}
+
+def preStage(stageName) { }
+
+def postStage(stageName) { }
+
+//--------
+// Stages
+//--------
+def runStage(stageName, config) { }
+
+//---------
+// Helpers
+//---------
+
+return this;
